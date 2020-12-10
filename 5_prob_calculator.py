97b0dd81738306548763932ccd5adf7678a50737
hjs429 <hjs928@naver.com>
2020-12-10 18:12:50 +0900

    feat: Finish this project Probability calculation function was added.

--- 5_prob_calculator.py
+++ 5_prob_calculator.py
@@ -22,60 +22,56 @@ class Hat:
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 
-  new_hat_contents = []
+    #create new_hat_contents variable
 
-  # move hat.contents to new_hat_contents
+    new_hat_contents=[]
 
-  for i in hat.contents:
-    new_hat_contents.append(i)
-
-  # find count of expected balls
-
-  ball_count = 0
-
-  for i in range(num_experiments):
-    # choose random balls
-    chosen_balls = []
-    if num_balls_drawn > len(new_hat_contents):
-      return 1.0
-
-    else:
-
-      for i in range(num_balls_drawn):
-        chose_ball = random.choice(new_hat_contents)
-        chosen_balls.append(chose_ball)
-        new_hat_contents.remove(chose_ball)
-
-
-      # replace new_hat_contents
-
-      new_hat_contents = []
-      for i in hat.contents:
+    for i in hat.contents:
         new_hat_contents.append(i)
+        
+    ball_count = 0
+
+    for i in range(num_experiments):
+
+        # create choosen_balls variable to check probability
+
+        choosen_balls = []
+
+        if(num_balls_drawn>=len(new_hat_contents)):
+            return 1.0
+
+        else:
+            for i in range(num_balls_drawn):
+                choose_ball = random.choice(new_hat_contents)
+                choosen_balls.append(choose_ball)
+                new_hat_contents.remove(choose_ball)
+
+        #init new_hat_contents
+
+        new_hat_contents=[]
+
+        for i in hat.contents:
+            new_hat_contents.append(i)
+        
+        color_count = 0
+
+        for key,value in expected_balls.items():
+            if(choosen_balls.count(key))>=value:
+               color_count+=1
+
+        if(color_count==len(expected_balls)):
+            ball_count+=1
+
+    return ball_count/num_experiments
 
 
-      # count the expected colors in chosen balls
-
-      color_count = 0
-
-      for key, value in expected_balls.items():
-        if chosen_balls.count(key) >= value:
-          color_count += 1
-
-
-      # check if color count equals the expected ball colors
-
-      if color_count == len(expected_balls):
-        ball_count += 1
-
-
-  return ball_count/num_experiments
-
-hat =Hat(red=5,blue=2)
-print(hat.draw(2))
 
 
 
 
 
-    
+      
+
+
+ 
+  
